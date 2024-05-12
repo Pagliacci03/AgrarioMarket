c0bc7cac0d26f707cfcc658f87be365d0a988864
rodrigodiazmat <rodrigo.diaz.m@ug.uchile.cl>
2024-05-12 19:12:09 -0400

    Añade boton para volver al indice

--- app.py
+++ app.py
@@ -58,48 +58,53 @@ def comunas():
 def agregarProducto():
 
     if request.method == "POST":
-        # datos del usuario registrados en el formulario
-        product_type = request.form.get("type")
-        products = request.form.getlist("product_checkbox")
-        description = request.form.get("description")
-        files = request.files.getlist("image")
-        region = request.form.get("region")
-        comuna = request.form.get("comuna")
-        productor_name = request.form.get("name")
-        productor_email = request.form.get("email")
-        phone_number = request.form.get("phone")
+        index = request.form.get("back_to_index")
 
-        # no tomar en cuenta al input de tipo file que se agrega después de agregar un archivo al input anterior
-        if len(files) >= 2 and len(files) <= 3 and not filetype.guess(files[-1]):
-            files = files[:-1] 
+        if not index:
+            # datos del usuario registrados en el formulario
+            product_type = request.form.get("type")
+            products = request.form.getlist("product_checkbox")
+            description = request.form.get("description")
+            files = request.files.getlist("image")
+            region = request.form.get("region")
+            comuna = request.form.get("comuna")
+            productor_name = request.form.get("name")
+            productor_email = request.form.get("email")
+            phone_number = request.form.get("phone")
 
-        if vald.validate_agregar_producto(product_type, products, description, files, region, comuna, productor_name, productor_email, phone_number):
-            comuna_id = db.get_id_comuna_by_nombre(comuna)
-            product_id = db.create_producto(product_type, description, comuna_id, productor_name, productor_email, phone_number)
+            # no tomar en cuenta al input de tipo file que se agrega después de agregar un archivo al input anterior
+            if len(files) >= 2 and len(files) <= 3 and not filetype.guess(files[-1]):
+                files = files[:-1] 
 
-            # cada producto se asocia a un tipo de verdura o fruta
-            for product in products:
-                name_product_id = db.get_id_tvf_by_nombre(product)
-                db.create_pvf(product_id, name_product_id)
+            if vald.validate_agregar_producto(product_type, products, description, files, region, comuna, productor_name, productor_email, phone_number):
+                comuna_id = db.get_id_comuna_by_nombre(comuna)
+                product_id = db.create_producto(product_type, description, comuna_id, productor_name, productor_email, phone_number)
 
-            # se suben las imagenes a una carpeta y base de datos en el servidor
-            for img in files:
-                # generación de un nombre aleatorio para el archivo
-                _filename = hashlib.sha256(
-                    secure_filename(img.filename).encode("utf-8")
-                    ).hexdigest()
-                _extension = filetype.guess(img).extension
-                img_filename = f"{_filename}_{str(uuid.uuid4())}.{_extension}"
+                # cada producto se asocia a un tipo de verdura o fruta
+                for product in products:
+                    name_product_id = db.get_id_tvf_by_nombre(product)
+                    db.create_pvf(product_id, name_product_id)
 
-                # guardar la imagen en una carpeta
-                img_path = os.path.join(app.config["UPLOAD_FOLDER"], img_filename)
-                img.save(img_path)
+                # se suben las imagenes a una carpeta y base de datos en el servidor
+                for img in files:
+                    # generación de un nombre aleatorio para el archivo
+                    _filename = hashlib.sha256(
+                        secure_filename(img.filename).encode("utf-8")
+                        ).hexdigest()
+                    _extension = filetype.guess(img).extension
+                    img_filename = f"{_filename}_{str(uuid.uuid4())}.{_extension}"
 
-                # guardar los datos del archivo en la base de datos
-                db.create_image(img_path, img_filename, product_id)
+                    # guardar la imagen en una carpeta
+                    img_path = os.path.join(app.config["UPLOAD_FOLDER"], img_filename)
+                    img.save(img_path)
 
-            return redirect(url_for("index"))
+                    # guardar los datos del archivo en la base de datos
+                    db.create_image(img_path, img_filename, product_id)
+
+                return redirect(url_for("index"))
         
+        else: # se presiono el boton de regreso al indice
+            return redirect(url_for("index"))
 
     regiones = []
     for name in db.get_regiones():
